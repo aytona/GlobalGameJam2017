298c76a87b3e0df4ecb581c97243412584d66f36
Christopher Aytona <christopher.aytona@gmail.com>
2017-01-21 16:58:22 -0500

    Endless Terrain

---
+++ Assets/Scripts/Chris/MovingTexture.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Chris {
+
+    public class MovingTexture : MonoBehaviour {
+        public Vector2 m_Direction;
+        public Texture m_Texture;
+        public Renderer m_Renderer;
+
+        void Start() {
+            m_Renderer = GetComponent<Renderer>();
+        }
+
+        void Update() {
+            m_Renderer.material.mainTextureOffset = new Vector2(m_Direction.x, m_Direction.y) + m_Renderer.material.mainTextureOffset;
+        }
+    }
+}
