1bfbf647d7bb6c88b784adf0f2f02c87b45c1804
jmontene <josmontenegro2012@gmail.com>
2017-01-21 01:43:14 -0500

    Need to optimize pathing

--- Assets/Script/Wave.cs
+++ Assets/Script/Wave.cs
@@ -1,19 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class Wave : MonoBehaviour {
 
+    public List<Transform> waypoints;
+    public float rotateDistance;
+    public float rotateSpeed;
+
     private NavMeshAgent agent;
-    private Transform currentTarget;
+    private int currentWaypoint = 0;
+
+    private Vector3 rotateDirection;
+    private Quaternion lookRotation;
 
 	// Use this for initialization
 	void Start () {
         agent = GetComponent<NavMeshAgent>();
+        agent.destination = waypoints[currentWaypoint].position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        agent.Move(new Vector3(0.1f, 0.1f, 0.1f));
-	}
+        if(!agent.pathPending && agent.remainingDistance <= rotateDistance){
+            agent.destination = waypoints[++currentWaypoint % waypoints.Count].position;
+        }
+    }
 }
