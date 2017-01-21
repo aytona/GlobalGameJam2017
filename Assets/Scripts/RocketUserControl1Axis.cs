91a367f034ea5cd83ffb53fb808ec1f2596657c5
Derek Hendrickx <derek@hendrickx.me>
2017-01-21 00:22:59 -0500

    Remove the mobile inputs

--- Assets/Scripts/RocketUserControl1Axis.cs
+++ Assets/Scripts/RocketUserControl1Axis.cs
@@ -4,9 +4,6 @@ using UnityStandardAssets.CrossPlatformInput;
 using UnityStandardAssets.Vehicles.Aeroplane;
 
 public class RocketUserControl1Axis : MonoBehaviour {
-	// these max angles are only used on mobile, due to the way pitch and roll input are handled
-	public float maxRollAngle = 80;
-	public float maxPitchAngle = 80;
 
 	// reference to the aeroplane that we're controlling
 	private AeroplaneController m_Aeroplane;
@@ -30,27 +27,9 @@ public class RocketUserControl1Axis : MonoBehaviour {
 
 		// auto throttle up, or down if braking.
     	m_Throttle = m_AirBrakes ? -1 : 1;
-#if MOBILE_INPUT
-	AdjustInputForMobileControls(ref roll, ref pitch, ref m_Throttle);
-#endif
+
 		// Pass the input to the aeroplane
 		m_Aeroplane.Move(0.0f, 0.0f, m_Yaw, m_Throttle, m_AirBrakes);
 	}
 
-
-	private void AdjustInputForMobileControls(ref float roll, ref float pitch, ref float throttle)
-	{
-		// because mobile tilt is used for roll and pitch, we help out by
-		// assuming that a centered level device means the user
-		// wants to fly straight and level!
-
-		// this means on mobile, the input represents the *desired* roll angle of the aeroplane,
-		// and the roll input is calculated to achieve that.
-		// whereas on non-mobile, the input directly controls the roll of the aeroplane.
-
-		float intendedRollAngle = roll*maxRollAngle*Mathf.Deg2Rad;
-		float intendedPitchAngle = pitch*maxPitchAngle*Mathf.Deg2Rad;
-		roll = Mathf.Clamp((intendedRollAngle - m_Aeroplane.RollAngle), -1, 1);
-		pitch = Mathf.Clamp((intendedPitchAngle - m_Aeroplane.PitchAngle), -1, 1);
-	}
 }
